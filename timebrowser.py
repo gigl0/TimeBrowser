31eb22c0009d9a33a06c2715c524a5cc3bb76fb4
gigl0 <iglis.gjoni@edu-its.it>
2025-11-20 13:36:30 +0000

    small fix

--- timebrowser.py
+++ timebrowser.py
@@ -3,10 +3,10 @@ from utils.gui_interface import TimeBrowserApp
 from utils.db_handler import init_db
 
 if __name__ == "__main__":
-    # 1. Inizializza il database (crea il file se non c'è)
+    # 1. Inizializza il database
     init_db()
     
-    # 2. Avvia la GUI (+2 punti)
+    # 2. Avvia la GUI  
     root = tk.Tk()
     app = TimeBrowserApp(root)
     root.mainloop()
